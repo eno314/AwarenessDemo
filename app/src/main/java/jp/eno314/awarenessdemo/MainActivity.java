083335c58117504b7b702f71c42ccbb778e9d7a3
Hiroto Kitamura <kirikonknow@gmail.com>
2016-12-04 10:32:30 +0900

    awareness api に接続する

--- app/src/main/java/jp/eno314/awarenessdemo/MainActivity.java
+++ app/src/main/java/jp/eno314/awarenessdemo/MainActivity.java
@@ -3,11 +3,19 @@ package jp.eno314.awarenessdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleApiClient client = new GoogleApiClient.Builder(getApplicationContext())
+                .addApi(Awareness.API)
+                .build();
+        client.connect();
     }
 }
