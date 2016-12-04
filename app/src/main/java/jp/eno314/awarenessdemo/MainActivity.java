860902e06c0b721f4da02120518a60a5028a10b1
Hiroto Kitamura <kirikonknow@gmail.com>
2016-12-04 14:59:53 +0900

    getDetectedActivity を使ってみた

--- app/src/main/java/jp/eno314/awarenessdemo/MainActivity.java
+++ app/src/main/java/jp/eno314/awarenessdemo/MainActivity.java
@@ -1,10 +1,16 @@
 package jp.eno314.awarenessdemo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.awareness.snapshot.DetectedActivityResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
                 .addApi(Awareness.API)
                 .build();
         client.connect();
+
+        Awareness.SnapshotApi.getDetectedActivity(client)
+                .setResultCallback(new ResultCallback<DetectedActivityResult>() {
+                    @Override
+                    public void onResult(@NonNull DetectedActivityResult detectedActivityResult) {
+                        if (!detectedActivityResult.getStatus().isSuccess()) {
+                            Log.e("BBBBB", "Could not get the current activity.");
+                            return;
+                        }
+                        ActivityRecognitionResult ar = detectedActivityResult.getActivityRecognitionResult();
+                        DetectedActivity probableActivity = ar.getMostProbableActivity();
+                        Log.i("BBBBB", probableActivity.toString());
+                    }
+                });
     }
 }
